74546c0b819a50a17bab4c692f2e4430c5cff7df
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2004-10-13 20:25:43 +0000

    ..
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@178 8c291193-6614-0410-a08d-adc6e07170bb

--- src/makedep/bldw32.cmd
+++ src/makedep/bldw32.cmd
@@ -1,2 +1,2 @@
-cl /MD /W3 /DWIN32 /DNO_X11 /DOBJSUFFIX=\".obj\" /FekDepCCxx.exe /Ox /Ob8 /G7 *.c
+cl /MD /W3 /DWIN32 /DNO_X11 /DOBJSUFFIX=\".obj\" /FekDepCCxx.exe /Ox /G7 *.c
 rem cl /MD /W3 /DDEBUG /DWIN32 /DNO_X11 /DOBJSUFFIX=\".obj\" /Zi /FekDepCCxx.exe *.c 
