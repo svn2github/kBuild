e5468c26d81ffdbda94b5bb918d0ae4318385907
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2005-04-21 16:54:02 +0000

    Win32.
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@259 8c291193-6614-0410-a08d-adc6e07170bb

--- src/kDepPre/kDepPre.c
+++ src/kDepPre/kDepPre.c
@@ -28,11 +28,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #ifdef HAVE_FGETC_UNLOCKED 
 # define FGETC(s)   getc_unlocked(s)
 #else
 # define FGETC(s)   fgetc(s)
+#endif 
+
+#ifdef NEED_ISBLANK
+# define isblank(ch) ( (unsigned char)(ch) == ' ' || (unsigned char)(ch) == '\t' )
 #endif 
 
 
