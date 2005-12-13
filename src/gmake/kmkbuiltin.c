e2c726935e4c11f39d900e7e0350146fbf5d15b0
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2005-12-13 04:46:52 +0000

    Adding builtin commands to MSC version.
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@362 8c291193-6614-0410-a08d-adc6e07170bb

--- src/gmake/kmkbuiltin.c
+++ src/gmake/kmkbuiltin.c
@@ -186,20 +186,24 @@ int kmk_builtin_command_parsed(int argc, char **argv)
      */
     if (!strcmp(pszCmd, "append"))
         rc = kmk_builtin_append(argc, argv, environ);
+#ifndef _MSC_VER
     else if (!strcmp(pszCmd, "cp"))
         rc = kmk_builtin_cp(argc, argv, environ);
     //else if (!strcmp(pszCmd, "chmod"))
     //    rc = kmk_builtin_chmod(argc, argv, environ);
+#endif
     else if (!strcmp(pszCmd, "echo"))
         rc = kmk_builtin_echo(argc, argv, environ);
     else if (!strcmp(pszCmd, "mkdir"))
         rc = kmk_builtin_mkdir(argc, argv, environ);
+#ifndef _MSC_VER
     //else if (!strcmp(pszCmd, "mv"))
     //    rc = kmk_builtin_mv(argc, argv, environ);
     else if (!strcmp(pszCmd, "rm"))
         rc = kmk_builtin_rm(argc, argv, environ);
     //else if (!strcmp(pszCmd, "rmdir"))
     //    rc = kmk_builtin_rmdir(argc, argv, environ);
+#endif
     else
     {
         printf("kmk_builtin: Unknown command '%s'!\n", pszCmd);
