0a407d41667ec81a08b022bedfd88f93ec850c41
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2007-05-25 20:28:17 +0000

    assert.h
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@925 8c291193-6614-0410-a08d-adc6e07170bb

--- src/gmakenew/function.c
+++ src/gmakenew/function.c
@@ -16,9 +16,6 @@ You should have received a copy of the GNU General Public License along with
 GNU Make; see the file COPYING.  If not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.  */
 
-#ifdef CONFIG_WITH_OPTIMIZATION_HACKS
-# include <assert.h>
-#endif
 #include "make.h"
 #include "filedef.h"
 #include "variable.h"
@@ -43,6 +40,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.  */
 #  include <limits.h>
 # endif
 #endif
+#include <assert.h> /* bird */
 
 
 struct function_table_entry
