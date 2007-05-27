6a85ea2a5ceff4c10b361c561b4a75b64b81079b
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2007-05-27 05:57:14 +0000

    oops.
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@948 8c291193-6614-0410-a08d-adc6e07170bb

--- src/kmk/kbuild.c
+++ src/kmk/kbuild.c
@@ -120,7 +120,7 @@ void init_kbuild(int argc, char **argv)
         szTmp[rc] == '\0';
 
 #elif defined(__OS2__)
-     _execname(g_szrtProgramPath, sizeof(g_szrtProgramPath));
+     _execname(tmp, GET_PATH_MAX);
      rc = 0;
 
 #elif defined(WINDOWS32)
