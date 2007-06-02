b6a9aa2673339ea8dac1339b1e2660088d8e45da
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2007-06-02 16:25:12 +0000

    forgot this.
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@1009 8c291193-6614-0410-a08d-adc6e07170bb

---
+++ src/lib/mytypes.h
@@ -0,0 +1,43 @@
+/* $Id$ */
+/** @file
+ *
+ * mytypes - wrapper that ensures the necessary uintXY_t types are defined.
+ *
+ * Copyright (c) 2007 knut st. osmundsen <bird-src-spam@anduin.net>
+ *
+ *
+ * This file is part of kBuild.
+ *
+ * kBuild is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * kBuild is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with kBuild; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef ___mytypes_h__
+#define ___mytypes_h__
+
+#include <stdlib.h>
+#include <sys/types.h>
+
+#if defined(_MSC_VER)
+typedef unsigned int uint32_t;
+typedef signed int int32_t;
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
+#else
+# include <stdint.h>
+#endif
+
+#endif 
+
