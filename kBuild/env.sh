2e0cd1fbb66b42a08f2f65d78c2f458030a2cf85
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2008-04-09 01:19:35 +0000

    Fixed hash bang line. Added an eval mode and shut up the debug info.
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@1510 8c291193-6614-0410-a08d-adc6e07170bb

--- kBuild/env.sh
+++ kBuild/env.sh
@@ -1,4 +1,4 @@
-#!/bin/sh should work fine too, just needs testing.
+#!/bin/sh
 # $Id$
 ## @file
 #
@@ -27,6 +27,30 @@
 #set -x
 
 #
+# Check if we're in eval mode or not.
+#
+ERR_REDIR=1
+DBG_REDIR=1
+EVAL_OPT=
+DBG_OPT=
+QUIET_OPT=
+if test "$1" = "--debug"; then
+    DBG_OPT="true"
+    shift
+fi
+if test "$1" = "--quiet"; then
+    QUIET_OPT="true"
+    shift
+fi
+if test "$1" = "--eval"; then
+    EVAL_OPT="true"
+    ERR_REDIR=2
+    DBG_REDIR=2
+    shift
+fi
+
+
+#
 # Determin the kBuild path from the script location.
 #
 if test -z "$PATH_KBUILD"; then
@@ -34,12 +58,12 @@ if test -z "$PATH_KBUILD"; then
     PATH_KBUILD=`cd "$PATH_KBUILD" ; /bin/pwd`
 fi
 if test ! -f "$PATH_KBUILD/footer.kmk" -o ! -f "$PATH_KBUILD/header.kmk" -o ! -f "$PATH_KBUILD/rules.kmk"; then
-    echo "$0: error: PATH_KBUILD ($PATH_KBUILD) is not pointing to a popluated kBuild directory.";
+    echo "$0: error: PATH_KBUILD ($PATH_KBUILD) is not pointing to a popluated kBuild directory." 1>&${ERR_REDIR}
     sleep 1;
     exit 1;
 fi
 export PATH_KBUILD
-echo "dbg: PATH_KBUILD=$PATH_KBUILD"
+test -n "$DBG_OPT" && echo "dbg: PATH_KBUILD=$PATH_KBUILD" 1>&${DBG_REDIR}
 
 
 #
@@ -49,7 +73,7 @@ if test -z "$BUILD_TYPE"; then
     BUILD_TYPE=release
 fi
 export BUILD_TYPE
-echo "dbg: BUILD_TYPE=$BUILD_TYPE"
+test -n "$DBG_OPT" && echo "dbg: BUILD_TYPE=$BUILD_TYPE" 1>&${DBG_REDIR}
 
 #
 # Determin the host platform.
@@ -94,14 +118,14 @@ if test -z "$BUILD_PLATFORM"; then
             ;;
 
         *)
-            echo "$0: unknown os $BUILD_PLATFORM"
+            echo "$0: unknown os $BUILD_PLATFORM" 1>&${ERR_REDIR}
             sleep 1
             exit 1
             ;;
     esac
 fi
 export BUILD_PLATFORM
-echo "dbg: BUILD_PLATFORM=$BUILD_PLATFORM"
+test -n "$DBG_OPT" && echo "dbg: BUILD_PLATFORM=$BUILD_PLATFORM" 1>&${DBG_REDIR}
 
 if test -z "$BUILD_PLATFORM_ARCH"; then
     # Try deduce it from the cpu if given.
@@ -172,7 +196,7 @@ if test -z "$BUILD_PLATFORM_ARCH"; then
             BUILD_PLATFORM_ARCH='alpha'
             ;;
 
-        *)  echo "$0: unknown cpu/arch - $BUILD_PLATFORM_ARCH"
+        *)  echo "$0: unknown cpu/arch - $BUILD_PLATFORM_ARCH" 1>&${ERR_REDIR}
             sleep 1
             exit 1
             ;;
@@ -180,13 +204,13 @@ if test -z "$BUILD_PLATFORM_ARCH"; then
 
 fi
 export BUILD_PLATFORM_ARCH
-echo "dbg: BUILD_PLATFORM_ARCH=$BUILD_PLATFORM_ARCH"
+test -n "$DBG_OPT" && echo "dbg: BUILD_PLATFORM_ARCH=$BUILD_PLATFORM_ARCH" 1>&${DBG_REDIR}
 
 if test -z "$BUILD_PLATFORM_CPU"; then
     BUILD_PLATFORM_CPU="blend"
 fi
 export BUILD_PLATFORM_CPU
-echo "dbg: BUILD_PLATFORM_CPU=$BUILD_PLATFORM_CPU"
+test -n "$DBG_OPT" && echo "dbg: BUILD_PLATFORM_CPU=$BUILD_PLATFORM_CPU" 1>&${DBG_REDIR}
 
 #
 # The target platform.
@@ -196,13 +220,13 @@ if test -z "$BUILD_TARGET"; then
     BUILD_TARGET="$BUILD_PLATFORM"
 fi
 export BUILD_TARGET
-echo "dbg: BUILD_TARGET=$BUILD_TARGET"
+test -n "$DBG_OPT" && echo "dbg: BUILD_TARGET=$BUILD_TARGET" 1>&${DBG_REDIR}
 
 if test -z "$BUILD_TARGET_ARCH"; then
     BUILD_TARGET_ARCH="$BUILD_PLATFORM_ARCH"
 fi
 export BUILD_TARGET_ARCH
-echo "dbg: BUILD_TARGET_ARCH=$BUILD_TARGET_ARCH"
+test -n "$DBG_OPT" && echo "dbg: BUILD_TARGET_ARCH=$BUILD_TARGET_ARCH" 1>&${DBG_REDIR}
 
 if test -z "$BUILD_TARGET_CPU"; then
     if test "$BUILD_TARGET_ARCH" = "$BUILD_PLATFORM_ARCH"; then
@@ -212,7 +236,7 @@ if test -z "$BUILD_TARGET_CPU"; then
     fi
 fi
 export BUILD_TARGET_CPU
-echo "dbg: BUILD_TARGET_CPU=$BUILD_TARGET_CPU"
+test -n "$DBG_OPT" && echo "dbg: BUILD_TARGET_CPU=$BUILD_TARGET_CPU" 1>&${DBG_REDIR}
 
 
 # Determin executable extension and path separator.
@@ -232,28 +256,30 @@ esac
 if test -z "$PATH_KBUILD_BIN"; then
     PATH_KBUILD_BIN="${PATH_KBUILD}/bin/${BUILD_PLATFORM}.${BUILD_PLATFORM_ARCH}"
 fi
-echo "dbg: PATH_KBUILD_BIN=${PATH_KBUILD_BIN} (not exported)"
+test -n "$DBG_OPT" && echo "dbg: PATH_KBUILD_BIN=${PATH_KBUILD_BIN} (not exported)" 1>&${DBG_REDIR}
 
 # Make shell. OS/2 and DOS only?
-export MAKESHELL="${PATH_KBUILD_BIN}/kmk_ash${_SUFF_EXE}";
+if test "$BUILD_PLATFORM" = "os2"; then
+    export MAKESHELL="${PATH_KBUILD_BIN}/kmk_ash${_SUFF_EXE}";
+fi
 
 #
 # Add the bin/x.y/ directory to the PATH.
 # NOTE! Once bootstrapped this is the only thing that is actually necessary.
 #
-PATH="${PATH_KBUILD_BIN}/${_PATH_SEP}$PATH"
+PATH="${PATH_KBUILD_BIN}${_PATH_SEP}$PATH"
 export PATH
-echo "dbg: PATH=$PATH"
+test -n "$DBG_OPT" && echo "dbg: PATH=$PATH" 1>&${DBG_REDIR}
 
 # Sanity and x bits.
 if test ! -d "${PATH_KBUILD_BIN}/"; then
-    echo "$0: warning: The bin directory for this platform doesn't exists. (${PATH_KBUILD_BIN}/)"
+    echo "$0: warning: The bin directory for this platform doesn't exists. (${PATH_KBUILD_BIN}/)" 1>&${ERR_REDIR}
 else
     for prog in kmk kDepPre kDepIDB kmk_append kmk_ash kmk_cat kmk_cp kmk_echo kmk_install kmk_ln kmk_mkdir kmk_mv kmk_rm kmk_rmdir kmk_sed;
     do
         chmod a+x ${PATH_KBUILD_BIN}/${prog} > /dev/null 2>&1
         if test ! -f "${PATH_KBUILD_BIN}/${prog}${_SUFF_EXE}"; then
-            echo "$0: warning: The ${prog} program doesn't exist for this platform. (${PATH_KBUILD_BIN}/${prog}${_SUFF_EXE})"
+            echo "$0: warning: The ${prog} program doesn't exist for this platform. (${PATH_KBUILD_BIN}/${prog}${_SUFF_EXE})" 1>&${ERR_REDIR}
         fi
     done
 fi
@@ -261,15 +287,29 @@ fi
 unset _SUFF_EXE
 unset _PATH_SEP
 
-# Execute command or spawn shell.
-if test $# -eq 0; then
-    echo "$0: info: Spawning work shell..."
-    if test "$TERM" != 'dumb'  -a  -n "$BASH"; then
-        export PS1='\[\033[01;32m\]\u@\h \[\033[01;34m\]\W \$ \[\033[00m\]'
-    fi
-    $SHELL -i
+if test -n "$EVAL_OPT"; then
+    test -n "$DBG_OPT" && echo "dbg: echoing exported variables" 1>&${DBG_REDIR}
+    echo "export BUILD_PLATFORM=${BUILD_PLATFORM}"
+    echo "export BUILD_PLATFORM_ARCH=${BUILD_PLATFORM_ARCH}"
+    echo "export BUILD_PLATFORM_CPU=${BUILD_PLATFORM_CPU}"
+    echo "export BUILD_TARGET=${BUILD_TARGET}"
+    echo "export BUILD_TARGET_ARCH=${BUILD_TARGET_ARCH}"
+    echo "export BUILD_TARGET_CPU=${BUILD_TARGET_CPU}"
+    echo "export BUILD_TYPE=${BUILD_TYPE}"
+    echo "export PATH_KBUILD=${PATH_KBUILD}"
+    echo "export PATH=${PATH}"
+    test -n "$DBG_OPT" && echo "dbg: finished" 1>&${DBG_REDIR}
 else
-    echo "$0: info: Executing command: $*"
-    $*
+    # Execute command or spawn shell.
+    if test $# -eq 0; then
+        test -z "${QUIET_OPT}" && echo "$0: info: Spawning work shell..." 1>&${ERR_REDIR}
+        if test "$TERM" != 'dumb'  -a  -n "$BASH"; then
+            export PS1='\[\033[01;32m\]\u@\h \[\033[01;34m\]\W \$ \[\033[00m\]'
+        fi
+        $SHELL -i
+    else
+        echo "$0: info: Executing command: $*"
+        $*
+    fi
 fi
 
