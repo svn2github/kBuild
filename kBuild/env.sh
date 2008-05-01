ad420075742d966c3e34f8ea709fa108fec6c8dc
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2008-05-01 15:34:39 +0000

    parisc and armv5tejl uname -m response.
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@1589 8c291193-6614-0410-a08d-adc6e07170bb

--- kBuild/env.sh
+++ kBuild/env.sh
@@ -343,14 +343,13 @@ if test -z "$KBUILD_HOST_ARCH"; then
         ia64)
             KBUILD_HOST_ARCH='ia64'
             ;;
-        #hppa32|hppa|parisc32|parisc)?
-        hppa32|parisc32)
+        hppa32|parisc32|parisc)
             KBUILD_HOST_ARCH='hppa32'
             ;;
         hppa64|parisc64)
             KBUILD_HOST_ARCH='hppa64'
             ;;
-        arm|armv4l|armv5tel)
+        arm|armv4l|armv5tel|armv5tejl)
             KBUILD_HOST_ARCH='arm'
             ;;
         alpha)
