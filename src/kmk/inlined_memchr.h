cb0247729ccdf3712f7cbc0c0a881875e52a6bb4
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2008-10-10 06:06:32 +0000

    kmk: inline memchr everywhere but on linux.
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@1813 8c291193-6614-0410-a08d-adc6e07170bb

---
+++ src/kmk/inlined_memchr.h
@@ -0,0 +1,58 @@
+#include <string.h>
+static __inline__ void *
+my_inline_memchr(const void *pv, int ch, register size_t cb)
+{
+    register const unsigned int   uch = (unsigned)ch;
+    register const unsigned char *pb = (const unsigned char *)pv;
+#if 1
+    while (cb >= 4)
+    {
+        if (*pb == uch)
+            return (unsigned char *)pb;
+        if (pb[1] == uch)
+            return (unsigned char *)pb + 1;
+        if (pb[2] == uch)
+            return (unsigned char *)pb + 2;
+        if (pb[3] == uch)
+            return (unsigned char *)pb + 3;
+        cb -= 4;
+        pb += 4;
+    }
+    switch (cb & 3)
+    {
+        case 0:
+            break;
+        case 1:
+            if (*pb == uch)
+                return (unsigned char *)pb;
+            break;
+        case 2:
+            if (*pb == uch)
+                return (unsigned char *)pb;
+            if (pb[1] == uch)
+                return (unsigned char *)pb + 1;
+            break;
+        case 3:
+            if (*pb == uch)
+                return (unsigned char *)pb;
+            if (pb[1] == uch)
+                return (unsigned char *)pb + 1;
+            if (pb[2] == uch)
+                return (unsigned char *)pb + 2;
+            break;
+    }
+
+#else
+    while (cb > 0)
+    {
+        if (*pb == uch)
+            return (void *)pb;
+        cb--;
+        pb++;
+    }
+#endif
+    return 0;
+}
+
+#define memchr my_inline_memchr
+
