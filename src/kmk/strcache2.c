cf917048ba6c69f7086a1242947300649e0bdb19
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2008-10-23 13:34:40 +0000

    build fix.
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@1916 8c291193-6614-0410-a08d-adc6e07170bb

--- src/kmk/strcache2.c
+++ src/kmk/strcache2.c
@@ -39,6 +39,9 @@
 typedef unsigned char  uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int   uint32_t;
+typedef signed char    int8_t;
+typedef signed short   int16_t;
+typedef signed int     int32_t;
 #else
 # include <stdint.h>
 #endif
