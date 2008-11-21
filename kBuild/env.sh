8dc3ded727cb6e859ba418911f905abbea121413
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2008-11-21 16:35:31 +0000

    sparcv9 == 64-bit sparc, older are 32-bit.
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@2098 8c291193-6614-0410-a08d-adc6e07170bb

--- kBuild/env.sh
+++ kBuild/env.sh
@@ -320,10 +320,10 @@ if test -z "$KBUILD_HOST_ARCH"; then
         x86|i86pc|ia32|i[3456789]86)
             KBUILD_HOST_ARCH='x86'
             ;;
-        sparc32|sparc)
+        sparc32|sparc|sparcv8|sparcv7|sparcv8e)
             KBUILD_HOST_ARCH='sparc32'
             ;;
-        sparc64)
+        sparc64|sparcv9)
             KBUILD_HOST_ARCH='sparc64'
             ;;
         s390)
