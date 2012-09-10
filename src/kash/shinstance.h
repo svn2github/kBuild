802c4797d4d580859482a60106e4adc1a5054023
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2012-09-10 20:57:58 +0000

    haiku fix
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@2657 8c291193-6614-0410-a08d-adc6e07170bb

--- src/kash/shinstance.h
+++ src/kash/shinstance.h
@@ -445,7 +445,7 @@ int sh_add_child(shinstance *psh, pid_t pid, void *hChild);
 #else
 #   include <sys/wait.h>
 #   ifdef __HAIKU__
-#       define WCOREDUMP(x) (0) /** @todo check the haiku headers and see if this information is available or not... */
+#       define WCOREDUMP(x) WIFCORED(x)
 #   endif
 #endif
 pid_t sh_fork(shinstance *);
