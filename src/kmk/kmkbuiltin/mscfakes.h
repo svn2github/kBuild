4a6916fed194f62fe422c08b681cd4d7eeec1ff3
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2017-10-11 17:55:09 +0000

    cmp.c,cmp_utils.c: 64-bit I/O on windows, please.
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@3092 8c291193-6614-0410-a08d-adc6e07170bb

--- src/kmk/kmkbuiltin/mscfakes.h
+++ src/kmk/kmkbuiltin/mscfakes.h
@@ -47,9 +47,11 @@
 #include <direct.h>
 #include "nt/ntstat.h"
 #include "nt/ntunlink.h"
-#if defined(MSC_DO_64_BIT_IO) && _MSC_VER >= 1400 /* We want 64-bit file lengths here when possible. */
-# define off_t __int64
-# define lseek _lseeki64
+#ifdef MSC_DO_64_BIT_IO
+# if _MSC_VER >= 1400 /* We want 64-bit file lengths here when possible. */
+#  define off_t __int64
+#  define lseek _lseeki64
+# endif
 #endif
 
 #undef timeval
