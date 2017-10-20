0f860084ea9af08cea7dba88dabd9411976e19a8
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2017-10-20 16:23:06 +0000

    env.sh: gnu/hurd machine name
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@3104 8c291193-6614-0410-a08d-adc6e07170bb

--- kBuild/env.sh
+++ kBuild/env.sh
@@ -338,7 +338,7 @@ if test -z "$KBUILD_HOST_ARCH"; then
         x86_64|AMD64|amd64|k8|k8l|k9|k10)
             KBUILD_HOST_ARCH='amd64'
             ;;
-        x86|i86pc|ia32|i[3456789]86|BePC)
+        x86|i86pc|ia32|i[3456789]86|BePC|i[3456789]86-AT[3456789]86)
             KBUILD_HOST_ARCH='x86'
             ;;
         alpha)
